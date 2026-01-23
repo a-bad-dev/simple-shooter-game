db2fa4d7dd81cebbae76ae1f347fa63d182cefbd
a-bad-dev <244852891+a-bad-dev@users.noreply.github.com>
2026-01-23 02:02:23 -0400

    Updates
    - Minor terrain changes to forest-4
    - All classes now spawn with a torch in forest-4
    - [Wielded Light](https://content.luanti.org/packages/bell07/wielded_light/) is now a part of the game

--- mods/game/maps/maps/forest-4/map.lua
+++ mods/game/maps/maps/forest-4/map.lua
@@ -16,7 +16,24 @@ local map_data = {
         on_start = "for x=0, 189 do\nfor y=0, 10 do\nfor z=0, 154 do\ncore.set_node({x=x,y=69+y,z=z}, {name=\"air\"})\nend\nend\nend",
         on_barrier_remove = "",
         on_end = ""
-   }
+   },
+
+	classes = {
+		class_1 = {
+			name = "Long-range", 
+			initial_items = {"ctf_ranged:m200_loaded", "default:sword_stone", "ctf_ranged:ammo 99", "default:torch 1"}
+		},
+
+		class_2 = {
+			name = "Mid-ranged",
+			initial_items = {"ctf_ranged:ak47_loaded", "ctf_ranged:glock17_loaded", "ctf_ranged:ammo 99", "default:torch 1"}
+		},
+
+		class_3 = {
+			name = "Short-range",
+			initial_items = {"ctf_ranged:benelli_loaded", "ctf_ranged:glock17_loaded", "ctf_ranged:ammo 99", "default:torch 1"}
+		}
+	}
 }
 
 return map_data
